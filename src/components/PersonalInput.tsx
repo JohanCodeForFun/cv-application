6946007436efa5f43d09529085e11f0ba803febb
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-22 23:11:37 +0200

    add display for error message

--- src/components/PersonalInput.tsx
+++ src/components/PersonalInput.tsx
@@ -17,7 +17,11 @@ const schema: ZodType<FormData> = z.object({
 
 export default function PersonalInput() {
 
-  const { register, handleSubmit } = useForm<FormData>({ resolver: zodResolver(schema)})
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({ resolver: zodResolver(schema)})
 
   const submitData = (data: FormData) => {
     console.log('IT WORKED', data)
@@ -28,18 +32,22 @@ export default function PersonalInput() {
       <div className="form-fill">
       <label>First Name:</label>
       <input type="text" {...register('firstName')}/>
+      {errors.firstName && <div className="form-message"><p>{errors.firstName.message}</p></div>}
       </div>
       <div className="form-fill">
       <label>Last Name:</label>
       <input type="text" {...register('lastName')}/>
+      {errors.firstName && <div className="form-message">{errors.lastName?.message}</div>}
       </div>
       <div className="form-fill">
       <label>Email:</label>
       <input type="text" {...register('email')}/>
+      {errors.firstName && <div className="form-message">{errors.email?.message}</div>}
       </div>
       <div className="form-fill">
       <label>Phone Number:</label>
       <input type="number" {...register('phoneNumber', { valueAsNumber: true})}/>
+      {errors.firstName && <div className="form-message">{errors.phoneNumber?.message}</div>}
       </div>
       <div>
         <input type="submit" />
