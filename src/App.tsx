da22047199a47f6978395d50d6463837fc4e2490
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-22 17:18:16 +0200

    play around and figure out how inputs work with useState and TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import PersonalInput from "./components/PersonalInput";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
   return (
     <>
       <h1>CV Application</h1>
-      <Input />
-      <Button>Click me!</Button>
+      <PersonalInput />
+      <Input
+        value={input}
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+      />
+      <Button onClick={() => console.log(input)}>Click me!</Button>
     </>
   );
 }
---
+++ src/components/PersonalInput.tsx
@@ -0,0 +1,5 @@
+export default function PersonalInput() {
+  return (
+    <p>hello</p>
+  )
+}
