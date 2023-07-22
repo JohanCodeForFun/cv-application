8f8efe28f85da9b7ca34bb8e8270612ede049ffe
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-22 16:54:50 +0200

    add example shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Button } from "./components/ui/button"
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
-
   return (
     <>
       <h1>CV Application</h1>
+      <Input />
       <Button>Click me!</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
