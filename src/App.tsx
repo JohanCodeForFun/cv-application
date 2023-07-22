45b834f045c6469d572daad38e631c33114f4480
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-22 23:01:28 +0200

    comment out not used code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
+// import { useState } from "react";
+// import { Button } from "./components/ui/button";
+// import { Input } from "./components/ui/input";
 import "./styles.css";
 // import {
 //   Form,
@@ -12,17 +12,18 @@ import "./styles.css";
 //   FormMessage,
 // } from "@/components/ui/form"
 
-// import PersonalInput from "./components/PersonalInput";
-import ProfileForm from "./components/ProfileForm";
+import PersonalInput from "./components/PersonalInput";
+// import ProfileForm from "./components/ProfileForm";
 
 function App() {
-  const [input, setInput] = useState("");
+  // const [input, setInput] = useState("");
   return (
     <>
       <div className="app-container">
         <div className="form-input">
-          <h1>CV Application</h1>
-          <ProfileForm />
+          <h1 className="text-center">CV Application</h1>
+          {/* <ProfileForm /> */}
+          <PersonalInput />
         </div>
       </div>
     </>
