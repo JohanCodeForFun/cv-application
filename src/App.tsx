a6cf5b5e7f7cdfccc307f8f2d77d712bcf19e0c4
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-22 17:48:10 +0200

    setup simple styling and forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
+import './styles.css'
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form"
+
 import PersonalInput from "./components/PersonalInput";
-import { useState } from "react";
+// import ProfileForm from "./components/ProfileForm";
 
 function App() {
   const [input, setInput] = useState("");
   return (
     <>
+    <div className="app-container">
+      {/* <ProfileForm /> */}
+      {/* <PersonalInput /> */}
+
+      <form className="form-input">
       <h1>CV Application</h1>
-      <PersonalInput />
-      <Input
-        value={input}
-        onChange={(event) => {
-          setInput(event.target.value);
-        }}
-      />
-      <Button onClick={() => console.log(input)}>Click me!</Button>
+        <label>hello</label>
+        <Input
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+        />
+        <Button onClick={() => console.log(input)}>Click me!</Button>
+      </form>
+      </div>
     </>
   );
 }
