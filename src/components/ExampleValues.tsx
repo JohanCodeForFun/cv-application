c1ee67a3f337da05218e09f6cb0d49ad19957893
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-23 21:49:56 +0200

    add skeleton comp for loading example cv data

---
+++ src/components/ExampleValues.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+export const ExampleValues = () => {
+  return (
+    <div>
+      <Button>Load example</Button>
+      <Button>clear values</Button>
+    </div>
+  )
+}
