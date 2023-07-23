3b4d8c3cb0bec6ace3a24a38687a154020bbe2d3
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-23 11:52:25 +0200

    lift state up to parent component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,44 @@ import "./styles.css";
 import PersonalInput from "./components/PersonalInput";
 // import ProfileForm from "./components/ProfileForm";
 
+import { useForm } from 'react-hook-form';
+import {z, ZodType} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: number;
+}
+const schema: ZodType<FormData> = z.object({
+  firstName: z.string().min(2).max(21),
+  lastName: z.string().min(2).max(21),
+  email: z.string().min(8).max(34).email(),
+  phoneNumber: z.number(),
+})
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>({ resolver: zodResolver(schema)})
+
+  const submitData = (data: FormData) => {
+    console.log('IT WORKED', data)
+  }
+
   return (
     <>
       <div className="app-container">
         <div className="form-input">
           <h1 className="text-center">CV Application</h1>
           {/* <ProfileForm /> */}
-          <PersonalInput />
+          <PersonalInput 
+          register={register}
+          handleSubmit={handleSubmit}
+          />
           <p>cv details below:</p>
           <p>...</p>
         </div>
--- src/components/PersonalInput.tsx
+++ src/components/PersonalInput.tsx
@@ -1,52 +1,28 @@
-import { useForm } from 'react-hook-form';
-import {z, ZodType} from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-
-type FormData = {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phoneNumber: number;
-}
-const schema: ZodType<FormData> = z.object({
-  firstName: z.string().min(2).max(21),
-  lastName: z.string().min(2).max(21),
-  email: z.string().min(8).max(34).email(),
-  phoneNumber: z.number(),
-})
 
 export default function PersonalInput() {
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<FormData>({ resolver: zodResolver(schema)})
-
-  const submitData = (data: FormData) => {
-    console.log('IT WORKED', data)
-  }
+ 
 
   return (
     <form onSubmit={handleSubmit(submitData)}>
       <div className="form-fill">
-      <label>First Name:</label>
-      <input type="text" {...register('firstName')}/>
+      <label htmlFor="first-name">First Name:</label>
+      <input type="text" name="first-name" {...register('firstName')}/>
       {errors.firstName && <div className="form-message"><p>{errors.firstName.message}</p></div>}
       </div>
       <div className="form-fill">
-      <label>Last Name:</label>
-      <input type="text" {...register('lastName')}/>
+      <label htmlFor="last-name">Last Name:</label>
+      <input type="text" name="last-name" {...register('lastName')}/>
       {errors.firstName && <div className="form-message">{errors.lastName?.message}</div>}
       </div>
       <div className="form-fill">
-      <label>Email:</label>
-      <input type="text" {...register('email')}/>
+      <label htmlFor="email">Email:</label>
+      <input type="email" name="email" {...register('email')}/>
       {errors.firstName && <div className="form-message">{errors.email?.message}</div>}
       </div>
       <div className="form-fill">
-      <label>Phone Number:</label>
-      <input type="number" {...register('phoneNumber', { valueAsNumber: true})}/>
+      <label htmlFor="phone-number">Phone Number:</label>
+      <input type="number" name="phone-number" {...register('phoneNumber', { valueAsNumber: true})}/>
       {errors.firstName && <div className="form-message">{errors.phoneNumber?.message}</div>}
       </div>
       <div>
