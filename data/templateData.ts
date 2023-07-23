81bf6da02dc93c9690f2cb1b0e70887d2cb12328
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-23 21:23:08 +0200

    add template data and assets

---
+++ data/templateData.ts
@@ -0,0 +1,19 @@
+const templates = [
+  {
+    name: "Classic CV",
+    src: "public/cv-classic.png",
+    desc: "Lorem ipsum"
+  },
+  {
+    name: "Contemporary CV",
+    src: "public/cv-contemporary.png",
+    desc: "Lorem ipsum"
+  },
+  {
+    name: "Bold CV",
+    src: "public/cv-bold.png",
+    desc: "Lorem ipsum"
+  },
+]
+
+export default templates;
