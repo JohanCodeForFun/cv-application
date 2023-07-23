466d646e942d223957d34cf7d9ff4e1e53774e0a
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-23 11:58:53 +0200

    remove excessive data

--- src/components/PersonalInput.tsx
+++ src/components/PersonalInput.tsx
@@ -7,22 +7,22 @@ export default function PersonalInput() {
     <form onSubmit={handleSubmit(submitData)}>
       <div className="form-fill">
       <label htmlFor="first-name">First Name:</label>
-      <input type="text" name="first-name" id="first-name" {...register('firstName')}/>
+      <input type="text" id="first-name" {...register('firstName')}/>
       {errors.firstName && <div className="form-message"><p>{errors.firstName.message}</p></div>}
       </div>
       <div className="form-fill">
       <label htmlFor="last-name">Last Name:</label>
-      <input type="text" name="last-name" id="last-name" {...register('lastName')}/>
+      <input type="text" id="last-name" {...register('lastName')}/>
       {errors.firstName && <div className="form-message">{errors.lastName?.message}</div>}
       </div>
       <div className="form-fill">
       <label htmlFor="email">Email:</label>
-      <input type="email" name="email" id="email" {...register('email')}/>
+      <input type="email" id="email" {...register('email')}/>
       {errors.firstName && <div className="form-message">{errors.email?.message}</div>}
       </div>
       <div className="form-fill">
       <label htmlFor="phone-number">Phone Number:</label>
-      <input type="number" name="phone-number" id="phone-number" {...register('phoneNumber', { valueAsNumber: true})}/>
+      <input type="number" id="phone-number" {...register('phoneNumber', { valueAsNumber: true})}/>
       {errors.firstName && <div className="form-message">{errors.phoneNumber?.message}</div>}
       </div>
       <div>
