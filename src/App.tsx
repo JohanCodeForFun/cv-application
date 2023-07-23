a4a0a8ce2b4d364560f38bab1bc91ce2019a550d
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-23 11:25:32 +0200

    clean up file and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 // import { useState } from "react";
-// import { Button } from "./components/ui/button";
-// import { Input } from "./components/ui/input";
 import "./styles.css";
-// import {
-//   Form,
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form"
 
 import PersonalInput from "./components/PersonalInput";
 // import ProfileForm from "./components/ProfileForm";
 
 function App() {
-  // const [input, setInput] = useState("");
   return (
     <>
       <div className="app-container">
