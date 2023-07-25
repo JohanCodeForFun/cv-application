9e52ef62e57f5989028d97e621a491e114fcffe2
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-25 11:44:59 +0200

    update copy for component

--- src/components/TemplateCV.tsx
+++ src/components/TemplateCV.tsx
@@ -4,14 +4,15 @@ import templates from "../../data/templateData"
 const TemplateCV = () => {
   return (
     <div className="template-container">
+      <p>Choose type of CV:</p>
       {templates.map((template) => {
         return (
           <div className="template" key={template.id}>
             <button>{template.name}</button>
             <img src={template.src} alt={template.name}/>
           </div>
-        );
-      })}
+        )
+      })};
     </div>
   );
 };
