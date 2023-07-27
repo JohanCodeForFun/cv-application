2699a4c467ac14863231c9cb9524bd87d0f3f631
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-27 21:41:30 +0200

    setup style module for landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import style from './landingpage.module.css'
 
-export const LandingPage = () => {
+function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div className={style.container}>
+      <div className={style.hero}>
+        <h2>Hero title</h2>
+        <img className={style.img} src="./vite.svg" alt="hero image" width={200} height={200}/>
+      </div>
+      <div className={style.info}>
+        <p>hello</p>
+      </div>
+    </div>
   )
 }
+
+export default LandingPage;
