b7fa9c8078531eca18ebb87af3ca6932c543fda2
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-07-30 22:16:47 +0200

    WIP fan the cards

--- src/components/CardTemplate.tsx
+++ src/components/CardTemplate.tsx
@@ -7,10 +7,12 @@ type Props = {
 
 function CardTemplate({ fullName, templateType }: Props) {
   return (
-    <div className={style.template}>
-      <h3>{fullName}</h3>
-      <p className={style[templateType]}>type, {templateType}</p>
+    <div className={style.templates}>
+      <div className={style.template}>
+        <h3>{fullName}</h3>
+        <p className={style[templateType]}>type, {templateType}</p>
       </div>
+    </div>
   )
 }
 
